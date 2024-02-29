2aafccf3d3066ddd3d91499abd355d2d87eb4286
Gustave Monce <gustave.monce@outlook.com>
2024-02-29 09:51:09 +0100

    Initialize ADB transport project

---
+++ AndroidDebugBridge/AndroidDebugBridgeTransport.cs
@@ -0,0 +1,74 @@
+﻿using MadWizard.WinUSBNet;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AndroidDebugBridge
+{
+    public class AndroidDebugBridgeTransport : IDisposable
+    {
+        private bool Disposed = false;
+        private readonly USBDevice USBDevice = null;
+        private USBPipe InputPipe = null;
+        private USBPipe OutputPipe = null;
+
+        public AndroidDebugBridgeTransport(string DevicePath)
+        {
+            USBDevice = new USBDevice(DevicePath);
+
+            foreach (USBPipe Pipe in USBDevice.Pipes)
+            {
+                if (Pipe.IsIn)
+                {
+                    InputPipe = Pipe;
+                }
+
+                if (Pipe.IsOut)
+                {
+                    OutputPipe = Pipe;
+                }
+            }
+
+            if (InputPipe == null || OutputPipe == null)
+            {
+                throw new Exception("Invalid USB device!");
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~AndroidDebugBridgeTransport()
+        {
+            Dispose(false);
+        }
+
+        public void Close()
+        {
+            USBDevice?.Dispose();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (Disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                // Other disposables
+            }
+
+            // Clean unmanaged resources here.
+            Close();
+
+            Disposed = true;
+        }
+    }
+}

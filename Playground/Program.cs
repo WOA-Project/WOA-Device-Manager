7956df21a05786b4f731e9381b61a1c5d12e59cc
Gustave Monce <gustave.monce@outlook.com>
2024-03-03 00:19:05 +0100

    fix: add twrp id for testing as well

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -6,7 +6,8 @@ namespace Playground
     {
         static void Main(string[] args)
         {
-            string usbid = @"\\?\USB#VID_045E&PID_0C26#0F0012E214600A#{dee824ef-729b-4a0e-9c14-b7117d33a817}";
+            string usbid = @"\\?\USB#VID_045E&PID_0C26#0F0012E214600A#{dee824ef-729b-4a0e-9c14-b7117d33a817}"; // Android (Duo 2)
+            //string usbid = @"\\?\USB#VID_18D1&PID_D001#0F0012E214600A#{dee824ef-729b-4a0e-9c14-b7117d33a817}"; // TWRP (Duo 2)
 
             Console.WriteLine($"Opening {usbid}...");
             using AndroidDebugBridgeTransport transport = new(usbid);
